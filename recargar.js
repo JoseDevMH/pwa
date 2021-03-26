e84d88ee3d8f41396f2e7367099ea258691ee2a7
Jose Garcia <josegarciaalvarez81@gmail.com>
2021-03-26 15:00:51 -0600

    Random y carga cada 2 seg

---
+++ recargar.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+    $(document).ready(function(){
+      setInterval(
+        function(){
+          $('#seccionRecargar').load('seccion.php');
+        },2000
+      );
+    });
+</script>
